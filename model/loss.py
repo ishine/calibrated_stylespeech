a07da4e5f560bd25474651f72c84c78dd707ef87
Nguyen Tan Dat <nguyentandat.work@gmail.com>
2021-10-01 16:58:00 +0700

    add forward_sum_loss + bin_loss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from .mi_upperbound import (
     CLUB
@@ -184,3 +185,52 @@ class   CalibratedStyleSpeechLoss(nn.Module):
             energy_loss,
             duration_loss,
         )
+
+
+class ForwardSumLoss(nn.Module):
+    def __init__(self, blank_logprob=-1):
+        super().__init__()
+        self.log_softmax = nn.LogSoftmax(dim=3)
+        self.ctc_loss = nn.CTCLoss(zero_infinity=True)
+        self.blank_prob = blank_logprob
+
+    def forward(self, attn_logprob, in_lens, out_lens):
+        """
+        This code is implemented base on 
+        https://github.com/NVIDIA/NeMo/blob/046fad0f8fa72ce67d0f7718a048f3be82526402/nemo/collections/tts/losses/aligner_loss.py#L24
+
+        attn_logprob: [B, S, T, D]
+        in_lens: [B]
+        out_lens: [B]
+
+        @return: forward_sum: loss
+        """
+
+        key_lens = in_lens
+        query_lens = out_lens
+        attn_logprob_padded = F.pad(input=attn_logprob, pad=(1, 0), value=self.blank_logprob)
+
+        total_loss = 0.0
+        for bid in range(attn_logprob.shape[0]):
+            target_seq = torch.arange(1, key_lens[bid] + 1).unsqueeze(0)
+            curr_logprob = attn_logprob_padded[bid].permute(1, 0, 2)[: query_lens[bid], :, : key_lens[bid] + 1]
+
+            curr_logprob = self.log_softmax(curr_logprob[None])[0]
+            loss = self.ctc_loss(
+                curr_logprob,
+                target_seq,
+                input_lengths=query_lens[bid : bid + 1],
+                target_lengths=key_lens[bid : bid + 1],
+            )
+            total_loss += loss
+
+        total_loss /= attn_logprob.shape[0]
+        return total_loss
+
+class BinLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, hard_attention, soft_attention):
+        log_sum = torch.log(soft_attention[hard_attention == 1] + 1e-16).sum()
+        return -log_sum / hard_attention.sum()
